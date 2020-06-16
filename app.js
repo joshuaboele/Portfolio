a6f6cc99784ee85b12559cd3a2fb3a4962d2d839
joshuaboele <Josboele@gmail.com>
2020-06-16 18:48:57 +0200

    Read more button fix, css cleanup and various other fixes

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ import { data } from "./data.js";
 const title = document.querySelector(".project__video--title");
 const description = document.querySelector(".project__description");
 const video = document.querySelector(".project__video");
-const projectButton = document.querySelectorAll(".project-navigation__button");
+const navigateButton = document.querySelectorAll(".project-navigation__button");
+const projects = document.querySelectorAll(".project");
 
-console.log(video.src);
+projects.forEach((project) => {
+    const toggleDescription = project.querySelector(".project__button");
+    const additionalInfo = project.querySelector(".project__more");
+
+    toggleDescription.addEventListener("click", function () {
+        additionalInfo.classList.toggle("is-visible");
+        toggleDescription.textContent = "Minimize description...";
+    });
+});
 
 // default value
 
 let index = 0;
 
-projectButton.forEach((el) => {
+navigateButton.forEach((el) => {
     el.addEventListener("click", function () {
         index = this.dataset.index;
         title.innerHTML = data[index].title;
